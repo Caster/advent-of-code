eea23449c62e4521a4267fe8bca6fe562ca52d41
Thom Castermans <thom.castermans@gmail.com>
2024-12-14 10:57:40 +0100

    Day 2024/12 part 2

--- src/main/java/com/github/caster/shared/map/ResettableMap.java
+++ src/main/java/com/github/caster/shared/map/ResettableMap.java
@@ -28,6 +28,35 @@ public final class ResettableMap {
                     .map(p -> new Cell(map.get(p), p.x(), p.y()));
         }
 
+        public long corners(final ResettableMap map) {
+            val hasSameValue = range(-1, 2)
+                    .mapToObj(dx -> range(-1, 2)
+                            .mapToObj(dy -> {
+                                val pos = new Position(x + dx, y + dy);
+                                if (!map.contains(pos))  return false;
+                                return map.get(pos) == value;
+                            })
+                            .toArray(Boolean[]::new)
+                    )
+                    .toArray(Boolean[][]::new);
+            return (
+                // "inner" corners
+                toInt(hasSameValue[1][0] && hasSameValue[0][1] && !hasSameValue[0][0]) +
+                toInt(hasSameValue[0][1] && hasSameValue[1][2] && !hasSameValue[0][2]) +
+                toInt(hasSameValue[1][2] && hasSameValue[2][1] && !hasSameValue[2][2]) +
+                toInt(hasSameValue[2][1] && hasSameValue[1][0] && !hasSameValue[2][0]) +
+                // "outer" corners
+                toInt(!(hasSameValue[0][1] || hasSameValue[1][0])) +
+                toInt(!(hasSameValue[0][1] || hasSameValue[1][2])) +
+                toInt(!(hasSameValue[2][1] || hasSameValue[1][0])) +
+                toInt(!(hasSameValue[2][1] || hasSameValue[1][2]))
+            );
+        }
+
+        private int toInt(final boolean b) {
+            return b ? 1 : 0;
+        }
+
     }
 
     public final int numRows;
@@ -57,16 +86,6 @@ public final class ResettableMap {
         val x = position.get(0);
         val y = position.get(1);
         return 0 <= x && x < numColumns && 0 <= y && y < numRows;
-    }
-
-    public void forEachCell(final CellVisitor visitor) {
-        for (int y = 0; y < numRows; y++) {
-            for (int x = 0; x < numColumns; x++) {
-                if (!visitor.continueVisit(get(y, x), y, x)) {
-                    return;
-                }
-            }
-        }
     }
 
     public Stream<Cell> stream() {
--- src/main/java/com/github/caster/solutions/y2024/Day12.java
+++ src/main/java/com/github/caster/solutions/y2024/Day12.java
@@ -1,8 +1,6 @@
 package com.github.caster.solutions.y2024;
 
 import com.github.caster.shared.BaseSolution;
-import com.github.caster.shared.map.Direction;
-import com.github.caster.shared.map.Position;
 import com.github.caster.shared.map.ResettableMap;
 import lombok.val;
 
@@ -54,73 +52,10 @@ public final class Day12 extends BaseSolution {
                     .sum();
         }
 
-        private long sides() {
-            if (plots.size() == 1) {
-                return 4;
-            }
-
-            // start at a corner
-            val possibleStartPlots = plots.stream()
-                    .filter(plot -> {
-                        val neighbors = plot.neighbors(map).filter(plots::contains).collect(toSet());
-                        if (neighbors.size() >= 3)  return false;
-                        if (neighbors.size() == 1)  return true;
-                        val nIt = neighbors.iterator();
-                        val n1 = nIt.next();
-                        val n2 = nIt.next();
-                        return n1.x() != n2.x() && n1.y() != n2.y();
-                    })
-                    .toList();
-            val visitedPlots = new HashSet<Position>();
-            return possibleStartPlots.stream().mapToLong(startPlot -> {
-                var currPos = new Position(startPlot.x(), startPlot.y());
-                if (!visitedPlots.add(currPos))  return 0L;
-                val startPos = currPos;
-
-                // walk in a direction along the perimeter
-                Direction currDir = null;
-                for (val direction : Direction.values()) {
-                    val movedPos = currPos.moved(direction);
-                    if (!map.contains(movedPos)) continue;
-                    if (plant != map.get(movedPos)) continue;
-
-                    val movedCcwPos = currPos.moved(direction.turnCounterclockwise());
-                    if (map.contains(movedCcwPos) && map.get(movedCcwPos) == plant) continue;
-
-                    currDir = direction;
-                    break;
-                }
-                if (currDir == null) throw new IllegalStateException("Could not find start direction");
-                val startDir = currDir;
-
-                var numSides = 0L;
-                currPos = currPos.moved(currDir);
-                do {
-                    visitedPlots.add(currPos);
-
-                    // can move CCW? then we ended a side, move there
-                    val ccwPos = currPos.moved(currDir.turnCounterclockwise());
-                    if (map.contains(ccwPos) && map.get(ccwPos) == plant) {
-                        numSides++;
-                        currDir = currDir.turnCounterclockwise();
-                        currPos = ccwPos;
-                        continue;
-                    }
-
-                    // can move forward? then keep doing so
-                    val fwPos = currPos.moved(currDir);
-                    if (map.contains(fwPos) && map.get(fwPos) == plant) {
-                        currPos = fwPos;
-                        continue;
-                    }
-
-                    // we found a side, turn CW
-                    numSides++;
-                    currDir = currDir.turnClockwise();
-                } while (!currPos.equals(startPos) || currDir != startDir);
-
-                return numSides;
-            }).sum();
+        private long corners() {
+            return plots.stream()
+                    .mapToLong(plot -> plot.corners(map))
+                    .sum();
         }
 
     }
@@ -160,7 +95,7 @@ public final class Day12 extends BaseSolution {
     @Override
     protected void part2() {
         System.out.println(regions.stream()
-                .mapToLong(region -> region.area() * region.sides())
+                .mapToLong(region -> region.area() * region.corners())
                 .sum());
     }
 
