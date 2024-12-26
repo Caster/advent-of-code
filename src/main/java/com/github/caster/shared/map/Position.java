90cbb1b9d73c144ea3657d374c1e9c837ffafe53
Thom Castermans <thom.castermans@gmail.com>
2024-12-26 09:25:13 +0100

    Day 2024/20

--- src/main/java/com/github/caster/shared/map/Position.java
+++ src/main/java/com/github/caster/shared/map/Position.java
@@ -1,9 +1,15 @@
 package com.github.caster.shared.map;
 
+import static java.lang.Math.abs;
+
 public record Position(
         int x,
         int y
 ) {
+
+    public long manhattanDistanceTo(final Position that) {
+        return abs(that.x - this.x) + abs(that.y - this.y);
+    }
 
     public Position moved(final Direction direction) {
         return switch (direction) {
