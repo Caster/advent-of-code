05366cbddb69a3143673c7d2fa06dcbc96788a7a
Thom Castermans <thom.castermans@gmail.com>
2024-12-27 18:02:00 +0100

    Day 2024/21 part 2

--- src/main/java/com/github/caster/solutions/y2024/Day21.java
+++ src/main/java/com/github/caster/solutions/y2024/Day21.java
@@ -4,12 +4,16 @@ import com.github.caster.shared.BaseSolution;
 import com.github.caster.shared.map.Position;
 import lombok.val;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.github.caster.shared.input.InputLoader.InputType.INPUT;
 import static java.lang.Long.parseLong;
 import static java.lang.Math.abs;
+import static java.util.Arrays.stream;
 import static java.util.Map.entry;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 public final class Day21 extends BaseSolution {
 
@@ -59,45 +63,33 @@ public final class Day21 extends BaseSolution {
             entry('>', new Position(2, 1))
     );
 
-    @Override
-    protected void part1() {
-        System.out.println(read.lines().mapToLong(code -> {
-            var sequence = findSequenceToType(code, NUMERIC_KEYPAD_POSITIONS);
-            for (var i = 0; i < 2; i++) {
-                sequence = findSequenceToType(sequence, DIRECTIONAL_KEYPAD_POSITIONS);
-            }
-            System.out.printf("%s: %-74s (%d * %d)%n", code, sequence, sequence.length(), parseLong(code.substring(0, 3)));
-            return parseLong(code.substring(0, 3)) * sequence.length();
-        }).sum());
+    record ButtonChange(char from, char to) {}
+
+    private static final Map<ButtonChange, String> NUMERIC_MOVES = findAllMovesOnKeyPad(NUMERIC_KEYPAD_POSITIONS);
+    private static final Map<ButtonChange, String> DIRECTIONAL_MOVES = findAllMovesOnKeyPad(DIRECTIONAL_KEYPAD_POSITIONS);
+
+    static Map<ButtonChange, String> findAllMovesOnKeyPad(final Map<Character, Position> keyPad) {
+        return keyPad.keySet().stream()
+                .filter(key -> key != ' ')
+                .flatMap(from -> keyPad.keySet().stream()
+                        .filter(key -> key != ' ' && key != from)
+                        .map(to -> new ButtonChange(from, to)))
+                .collect(toMap(identity(), buttonChange -> findSequenceToType(buttonChange, keyPad) + 'A'));
     }
 
-    String findSequenceToType(final String code, final Map<Character, Position> positionMap) {
-        val sequence = new StringBuilder();
-        var curPos = positionMap.get('A');
-        for (val buttonToPress : code.toCharArray()) {
-            var targetPos = positionMap.get(buttonToPress);
-            val xDiff = targetPos.x() - curPos.x();
-            val yDiff = targetPos.y() - curPos.y();
-            val h = (xDiff < 0 ? "<" : ">").repeat(abs(xDiff));
-            val v = (yDiff < 0 ? "^" : "v").repeat(abs(yDiff));
-            val verticalFirstCorner = new Position(curPos.x(), targetPos.y());
-            val horizontalFirstCorner = new Position(targetPos.x(), curPos.y());
-            val canGoVerticalFirst = !positionMap.get(' ').equals(verticalFirstCorner);
-            val canGoHorizontalFirst = !positionMap.get(' ').equals(horizontalFirstCorner);
-            sequence.append(whatToAppend(xDiff, yDiff, h, v, canGoVerticalFirst, canGoHorizontalFirst));
-            sequence.append('A');
-            curPos = targetPos;
-        }
-        return sequence.toString();
-    }
-
-    String whatToAppend(
-            final int xDiff, final int yDiff,
-            final String h, final String v,
-            final boolean canGoVerticalFirst, final boolean canGoHorizontalFirst
-    ) {
+    static String findSequenceToType(final ButtonChange buttonChange, final Map<Character, Position> keyPad) {
+        val curPos = keyPad.get(buttonChange.from());
+        val targetPos = keyPad.get(buttonChange.to());
+        val xDiff = targetPos.x() - curPos.x();
+        val h = (xDiff < 0 ? "<" : ">").repeat(abs(xDiff));
+        val yDiff = targetPos.y() - curPos.y();
+        val v = (yDiff < 0 ? "^" : "v").repeat(abs(yDiff));
         if (xDiff == 0)  return v;
         if (yDiff == 0)  return h;
+        val verticalFirstCorner = new Position(curPos.x(), targetPos.y());
+        val horizontalFirstCorner = new Position(targetPos.x(), curPos.y());
+        val canGoVerticalFirst = !keyPad.get(' ').equals(verticalFirstCorner);
+        val canGoHorizontalFirst = !keyPad.get(' ').equals(horizontalFirstCorner);
         if (!canGoVerticalFirst)  return h + v;
         if (!canGoHorizontalFirst)  return v + h;
         if (xDiff < 0 && yDiff < 0)
@@ -109,20 +101,55 @@ public final class Day21 extends BaseSolution {
         return v + h;     // prefer ^> over >^
     }
 
-    // TODO: some kind of memoization?
-    record MemoKey(Position curPos, Position endPos) {}
+    record MemoKey(String sequence, int numDirectionalRobots) {}
+
+    private static final Map<MemoKey, Long> DIRECTIONAL_SEQUENCE_CACHE = new HashMap<>();
+
+    @Override
+    protected void part1() {
+        System.out.println(
+                read.lines()
+                        .mapToLong(code -> determineLineComplexity(code, 2))
+                        .sum()
+        );
+    }
+
+    long determineLineComplexity(final String code, final int numDirectionalRobots) {
+        val sequence = findSequenceToType(code, NUMERIC_MOVES);
+        return parseLong(code.substring(0, 3)) * findSequenceLengthToTypeDirectional(sequence, numDirectionalRobots);
+    }
+
+    String findSequenceToType(final String code, final Map<ButtonChange, String> keyPadMoves) {
+        val sequence = new StringBuilder();
+        var fromButton = 'A';
+        for (var toButton : code.toCharArray()) {
+            // the moves maps do not store sequences to stay on the same button, so default to 'A'ctivating
+            sequence.append(keyPadMoves.getOrDefault(new ButtonChange(fromButton, toButton), "A"));
+            fromButton = toButton;
+        }
+        return sequence.toString();
+    }
+
+    long findSequenceLengthToTypeDirectional(final String sequence, final int numDirectionalRobots) {
+        if (numDirectionalRobots == 0)  return sequence.length();
+        val memoKey = new MemoKey(sequence, numDirectionalRobots);
+        val cachedSequenceLength = DIRECTIONAL_SEQUENCE_CACHE.get(memoKey);
+        if (cachedSequenceLength != null)  return cachedSequenceLength;
+        val sequenceLength = stream(sequence.split("(?<=A)"))
+                .map(move -> findSequenceToType(move, DIRECTIONAL_MOVES))
+                .mapToLong(moveSequence -> findSequenceLengthToTypeDirectional(moveSequence, numDirectionalRobots - 1))
+                .sum();
+        DIRECTIONAL_SEQUENCE_CACHE.put(memoKey, sequenceLength);
+        return sequenceLength;
+    }
 
     @Override
     protected void part2() {
-        System.out.println(read.lines().mapToLong(code -> {
-            var sequence = findSequenceToType(code, NUMERIC_KEYPAD_POSITIONS);
-            for (var i = 0; i < 2; i++) {
-                sequence = findSequenceToType(sequence, DIRECTIONAL_KEYPAD_POSITIONS);
-            }
-            System.out.printf("%s: %-74s (%d * %d)%n", code, sequence, sequence.length(), parseLong(code.substring(0, 3)));
-            return parseLong(code.substring(0, 3)) * sequence.length();
-        }).sum());
+        System.out.println(
+                read.lines()
+                        .mapToLong(code -> determineLineComplexity(code, 25))
+                        .sum()
+        );
     }
-
 
 }
