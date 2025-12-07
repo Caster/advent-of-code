2d6efed9b36c995250dfa0a18d30ffe3c24e2c33
Thom Castermans <ISC85979@politie.local>
2025-12-07 08:06:23 +0100

    Day 2025/01 Gatherer solution

--- src/main/java/com/github/caster/solutions/y2025/Day1.java
+++ src/main/java/com/github/caster/solutions/y2025/Day1.java
@@ -6,6 +6,7 @@ import lombok.val;
 
 import static com.github.caster.shared.input.InputLoader.InputType.INPUT;
 import static java.lang.Integer.parseInt;
+import static java.util.stream.Gatherers.fold;
 
 public final class Day1 extends BaseSolution {
 
@@ -16,29 +17,45 @@ public final class Day1 extends BaseSolution {
 
     public Day1() {
         read.from(INPUT);
-        dial = new Dial();
-        read.lines().forEach(dial::rotate);
+        dial = read.lines()
+                .map(Rotation::new)
+                .gather(fold(Dial::new, Dial::apply))
+                .findFirst().orElseThrow();
     }
 
-    private static final class Dial {
+    private record Rotation(
+            int offset,
+            int fullRotations
+    ) {
+        private Rotation(final String rotation) {
+            val direction = rotation.charAt(0) == 'R' ? 1 : -1;
+            val absoluteRotation = parseInt(rotation.substring(1));
+            this(
+                    (direction * absoluteRotation) % DIAL_SIZE,
+                    absoluteRotation / DIAL_SIZE
+            );
+        }
+    }
 
-        private int position = START;
-        private int stoppedAtZeroCount = 0;
-        private int passedZeroCount = 0;
-
-        void rotate(final String rotation) {
-            val absRot = parseInt(rotation.substring(1));
-            passedZeroCount += absRot / DIAL_SIZE;
-            val rot = (rotation.charAt(0) == 'R' ? 1 : -1) * absRot % DIAL_SIZE;
-            if (position > 0 && (-rot >= position || position + rot >= DIAL_SIZE)) {
-                passedZeroCount++;
-            }
-            position = (position + rot + DIAL_SIZE) % DIAL_SIZE;
-            if (position == 0) {
-                stoppedAtZeroCount++;
-            }
+    private record Dial(
+            int position,
+            int stoppedAtZeroCount,
+            int passedZeroCount
+    ) {
+        private Dial() {
+            this(START, 0, 0);
         }
 
+        Dial apply(final Rotation rotation) {
+            val newPositionRaw = position + rotation.offset;
+            val newPosition = (newPositionRaw + DIAL_SIZE) % DIAL_SIZE;
+            return new Dial(
+                    newPosition,
+                    stoppedAtZeroCount + (newPosition == 0 ? 1 : 0),
+                    passedZeroCount + rotation.fullRotations + ((position > 0
+                            && (newPositionRaw <= 0 || DIAL_SIZE <= newPositionRaw)) ? 1 : 0)
+            );
+        }
     }
 
     @Override
@@ -52,4 +69,3 @@ public final class Day1 extends BaseSolution {
     }
 
 }
-
