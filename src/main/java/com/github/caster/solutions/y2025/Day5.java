888d0f7e70d5832c53f23fce7a2188fb3fddc20e
Thom Castermans <ISC85979@politie.local>
2025-12-11 08:01:41 +0100

    Day 2025/05: immutable Interval; small refactoring

--- src/main/java/com/github/caster/solutions/y2025/Day5.java
+++ src/main/java/com/github/caster/solutions/y2025/Day5.java
@@ -1,18 +1,18 @@
 package com.github.caster.solutions.y2025;
 
-import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Gatherer;
 
 import com.github.caster.shared.BaseSolution;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.val;
 
 import static com.github.caster.shared.input.InputLoader.InputType.INPUT;
 import static com.github.caster.shared.input.InputLoader.parseLongs;
-import static java.util.stream.Gatherer.ofSequential;
+import static java.util.Comparator.comparingLong;
 
 public final class Day5 extends BaseSolution {
 
@@ -23,22 +23,16 @@ public final class Day5 extends BaseSolution {
         read.from(INPUT);
         read.with(sections -> {
             freshIngredientIdIntervals = sections.next().lines()
-                    .map(Interval::fromString).toList();
+                    .map(Interval::fromString)
+                    .sorted(comparingLong(Interval::start))
+                    .gather(new MergingIntervalGatherer())
+                    .toList();
             availableIngredients = sections.next().lines()
                     .mapToLong(Long::parseLong).boxed().toList();
         });
     }
 
-    @Getter
-    @AllArgsConstructor
-    private static final class Interval {
-
-        private long start;
-        private long end;
-
-        static Interval empty() {
-            return new Interval(0, -1);
-        }
+    private record Interval(long start, long end) {
 
         static Interval fromString(final String intervalString) {
             val ends = parseLongs(intervalString, "-");
@@ -56,6 +50,68 @@ public final class Day5 extends BaseSolution {
         long size() {
             return end - start + 1;
         }
+
+    }
+
+    private static final class MergingIntervalGatherer
+            implements Gatherer<Interval, MutableOptional<Interval>, Interval> {
+
+        @Override
+        public Supplier<MutableOptional<Interval>> initializer() {
+            return MutableOptional::new;
+        }
+
+        @Override
+        public Integrator<MutableOptional<Interval>, Interval, Interval> integrator() {
+            return (state, currInterval, downstream) -> {
+                if (state.isEmpty()) {
+                    state.set(currInterval);
+                    return true;
+                }
+
+                val prevInterval = state.get();
+                if (prevInterval.contains(currInterval)) {
+                    return true;
+                }
+                if (prevInterval.contains(currInterval.start)) {
+                    state.set(new Interval(prevInterval.start, currInterval.end));
+                    return true;
+                }
+                state.ifPresent(downstream::push).set(currInterval);
+                return true;
+            };
+        }
+
+        @Override
+        public BiConsumer<MutableOptional<Interval>, Downstream<? super Interval>> finisher() {
+            return (state, downstream) -> state.ifPresent(downstream::push);
+        }
+
+    }
+
+    private static final class MutableOptional<T> {
+
+        private T value = null;
+
+        T get() {
+            return value;
+        }
+
+        boolean isEmpty() {
+            return value == null;
+        }
+
+        MutableOptional<T> ifPresent(final Consumer<T> consumer) {
+            if (!isEmpty()) {
+                consumer.accept(value);
+            }
+            return this;
+        }
+
+        void set(final T newValue) {
+            this.value = newValue;
+        }
+
     }
 
     @Override
@@ -70,27 +126,7 @@ public final class Day5 extends BaseSolution {
 
     @Override
     protected void part2() {
-        val freshIngredientCount = freshIngredientIdIntervals.stream()
-                .sorted(Comparator.comparingLong(Interval::getStart))
-                .gather(ofSequential(
-                        Interval::empty,
-                        Gatherer.Integrator.<Interval, Interval, Interval>of((prevInterval, currInterval, downstream) -> {
-                            if (prevInterval.contains(currInterval)) {
-                                return true;
-                            }
-                            if (prevInterval.contains(currInterval.start)) {
-                                prevInterval.end = currInterval.end;
-                                return true;
-                            }
-                            downstream.push(prevInterval);
-                            prevInterval.start = currInterval.start;
-                            prevInterval.end = currInterval.end;
-                            return true;
-                        }),
-                        (prevInterval, downstream) -> downstream.push(prevInterval)
-                ))
-                .mapToLong(Interval::size)
-                .sum();
-        IO.println(freshIngredientCount);
+        IO.println(freshIngredientIdIntervals.stream().mapToLong(Interval::size).sum());
     }
+
 }
